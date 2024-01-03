4f2f4b947d796c63e733f08ad6b6b8ec0969b8ec
yawning5 <yawning055@gmail.com>
2024-01-03 18:54:31 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,11 +1,20 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        if (a != b && b != c & a != c) {
-            return a + b + c;
-        }
+        int exponent = 1;
+        int result = 1;
+
         if (a == b && b == c) {
-            return (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+            exponent++;
         }
-        return (a + b + c) * (a * a + b * b + c * c);
+
+        if (a == b || b == c || c == a) {
+            exponent++;
+        }
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= Math.pow(a, i) + Math.pow(b, i) + Math.pow(c, i);
+        }
+
+        return result;
     }
 }
