0256f58877e3466d37907746eab28c447c809d46
yawning5 <yawning055@gmail.com>
2024-01-08 00:33:00 +0900

    [level unrated] Title: 수 조작하기 1, Time: 1.61 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, String control) {
+        char[] charArray = control.toCharArray();
+        for (char ch : charArray) {
+            switch (ch) {
+                case 'w':
+                    n += 1;
+                    break;
+                case 's':
+                    n -= 1;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+            }
+        }
+        return n;
+    }
+}
