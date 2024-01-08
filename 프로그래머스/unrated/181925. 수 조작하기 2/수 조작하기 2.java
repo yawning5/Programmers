b80e15a9fb7570a9d59bf239494920f38092701e
yawning5 <yawning055@gmail.com>
2024-01-08 12:10:54 +0900

    [level unrated] Title: 수 조작하기 2, Time: 4.90 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i < numLog.length; i++) {
+            switch (numLog[i] - numLog[i - 1]) {
+                case 1:
+                    answer.append('w');
+                    break;
+                case -1:
+                    answer.append('s');
+                    break;
+                case 10:
+                    answer.append('d');
+                    break;
+                case -10:
+                    answer.append('a');
+                    break;
+            }
+        }
+        return answer.toString();
+    }
+}
