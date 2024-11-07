da2118bb955f95f3e49de8c2395847e0e7f9cc43
yawning5 <yawning055@gmail.com>
2024-11-07 13:44:16 +0900

    [level 0] Title: k의 개수, Time: 5004.67 ms, Memory: 381 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int answer = 0;
-        for(int s = i; s <= j; s++) {
-            char[] cur = String.valueOf(s).toCharArray();
-            for(char c : cur) {
-                if(Character.getNumericValue(c) == k) answer++;
-            }
+        String str = "";
+        for(int a = i; a <= j; a++) {
+            str += a+"";
         }
-        return answer;
+
+        return str.length() - str.replace(k+"", "").length();
     }
 }
