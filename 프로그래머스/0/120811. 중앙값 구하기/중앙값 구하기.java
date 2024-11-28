67e3fc41ee2a8a529beaca9a2da8afd12647ee73
yawning5 <yawning055@gmail.com>
2024-11-28 21:44:44 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.54 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        
+        return array[array.length / 2];
+    }
+}
