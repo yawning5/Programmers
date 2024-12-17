e5e460509494ce1982724802995aeec88135f67d
yawning5 <yawning055@gmail.com>
2024-12-17 14:17:45 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        int index = 0;
+        int[] possAngle = new int[6];
+        for (int i = 0; i < 3; i++) {
+            for (int j = i + 1; j < dots.length; j++) {
+                possAngle[index] = ((dots[i][0]*100 - dots[j][0]*100)) / (dots [i][1] - dots[j][1]);
+            index++;
+            }
+            
+        }
+
+        for (int i = 0; i < 2; i++) {
+            if (possAngle[i] == possAngle[possAngle.length - i - 1]) {
+                return 1;
+            }
+        }
+        
+        
+        
+        
+        return answer;
+    }
+}
