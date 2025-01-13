584998e24d28bac3f282a5e7922812b391cae66c
yawning5 <yawning055@gmail.com>
2025-01-13 21:19:12 +0900

    [level 1] Title: 문자열 나누기, Time: 1.12 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int countX = 0;
+        int countNotX = 0;
+        char x = s.charAt(0);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == x) {
+                countX++;
+            } else {
+                countNotX++;
+            }
+
+            if (countX == countNotX) {
+                answer++;
+                if (i + 1 < s.length()) {
+                    x = s.charAt(i + 1);
+                }
+                countX = 0;
+                countNotX = 0;
+            }
+        }
+
+        if (countX != 0 || countNotX != 0) {
+            answer++;
+        }
+        
+        return answer;
+    }
+}
