ba64f2cfbafe98986467f5de54e790e33ed6e25c
yawning5 <yawning055@gmail.com>
2025-05-19 12:44:02 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.11 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int[] A, int[] B)
+    {
+        int answer = 0;
+        int length = A.length;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for (int i = 0; i < length; i++) {
+            answer += A[i] * B[length - 1 - i];
+        }
+
+        return answer;
+    }
+}
