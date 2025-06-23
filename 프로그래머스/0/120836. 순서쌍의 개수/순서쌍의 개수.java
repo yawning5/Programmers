1f52c387514514973ab4e6ef58df76688a71e51f
yawning5 <yawning055@gmail.com>
2025-06-23 23:03:00 +0900

    [level 0] Title: 순서쌍의 개수, Time: 5.15 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i = 1;
+        while (i <= n) {
+            if (n % i == 0) {
+                answer++;
+            }
+            i++;
+        }
+        return answer;
+    }
+}
