9ac6e3e5d94bfe73acf3cfb736ec9da80694c923
yawning5 <yawning055@gmail.com>
2025-10-01 10:51:38 +0900

    [level 1] Title: 모의고사, Time: 3.69 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,40 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int highScore = 0;
-        List<Integer> highScoreList = new ArrayList<>();
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        Map<Integer, Integer> score = new HashMap<>();
-        score.put(1, 0);
-        score.put(2, 0);
-        score.put(3, 0);
-        int temp = 0;
-
-        for (int i : answers) {
-            if (i == one[temp % 5]) score.put(1, score.get(1) + 1);
-            if (i == two[temp % 8]) score.put(2, score.get(2) + 1);
-            if (i == three[temp% 10]) score.put(3, score.get(3) + 1);
-            temp++;
-        }
-
-        for (int i = 1; i < 4; i++) {
-            highScore = Math.max(score.get(i), highScore);
-        }
-
-        for (int i = 1; i < 4; i++) {
-            if (score.get(i) == highScore) {
-                highScoreList.add(i);
+        int[] score = {0, 0, 0};
+        int[] first = {1, 2, 3, 4, 5};
+        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == first[i % first.length]) {
+                score[0]++;
+            }
+            if (answers[i] == second[i % second.length]) {
+                score[1]++;
+            }
+            if (answers[i] == third[i % third.length]) {
+                score[2]++;
             }
         }
-
-        int[] answer = highScoreList.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
-
-        Arrays.sort(answer);
-
-        return answer;
+        
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        
+        List<Integer> result = new ArrayList<>();
+        for(int i =0; i < 3; i++) {
+            if(score[i] == maxScore) {
+                result.add(i + 1);
+            }
+        }
+        
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
